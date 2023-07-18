ca1c8d524d7d9c25e1ef3b2da6be626717165f9d
mertee05 <131835458+mertee05@users.noreply.github.com>
2023-07-18 11:46:36 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const inputField = document.getElementById("input-value");
+const lengthField = document.getElementById("length");
+const volumeField = document.getElementById("volume");
+const massField = document.getElementById("mass");
+let convertBtn = document.getElementById("convert-btn");
+
+convertBtn.addEventListener("click", function () {
+  console.log("debug");
+});
